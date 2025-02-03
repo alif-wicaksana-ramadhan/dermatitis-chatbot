e070c1403b799ec2c4173feef98c516ee9497485
alifwr <alifwr98@gmail.com>
2025-02-03 09:34:45 +0700

    update

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -22,25 +22,27 @@ const parseTextToSpan = (text: string) => {
 </script>
 
 <template>
-  <div class="container p-4 mx-auto flex flex-col h-screen items-center justify-evenly bg-yellow-100">
-    <span class="text-7xl">Dermatitis AI</span>
-    <div v-if="answer" class="flex items-center justify-center h-1/3">
-      <Buble :isLoading="isLoading" :text="parseTextToSpan(answer)" />
-    </div>
-    <div v-else class="flex items-center justify-center h-1/3">
-      <Icon v-if="isLoading" name="eos-icons:loading" size="300" style="color: black" />
-    </div>
-    <div class="flex items-center w-full justify-center bg-green-100">
-      <textarea
-          v-model="textInput"
-          :disabled="isLoading"
-          @keydown.enter.exact.prevent="submitMessage"
-          @keydown.enter.shift.exact="newLine"
-          placeholder="...."
-          class="w-full p-3 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-          rows="1"
-      ></textarea>
-      <button @click="submitMessage" class="p-2">Send</button>
+  <div class="p-4 mx-auto flex flex-col h-screen items-center justify-evenly bg-gray-100">
+    <div class="container flex flex-col h-screen items-center justify-evenly">
+      <span class="text-7xl">Dermatitis AI</span>
+      <div v-if="answer" class="flex items-center justify-center h-1/3">
+        <Buble :isLoading="isLoading" :text="parseTextToSpan(answer)" />
+      </div>
+      <div v-else class="flex items-center justify-center h-1/3">
+        <Icon v-if="isLoading" name="eos-icons:loading" size="300" style="color: black" />
+      </div>
+      <div class="flex items-center w-full justify-center">
+        <textarea
+            v-model="textInput"
+            :disabled="isLoading"
+            @keydown.enter.exact.prevent="submitMessage"
+            @keydown.enter.shift.exact="newLine"
+            placeholder="...."
+            class="w-full p-3 border rounded-tl-lg rounded-bl-lg h-full focus:outline-none focus:ring-2 focus:ring-blue-500"
+            rows="1"
+        ></textarea>
+        <button @click="submitMessage" :disabled="isLoading" class="p-2 h-full bg-blue-400 rounded-tr-lg rounded-br-lg">Send</button>
+      </div>
     </div>
   </div>
 </template>
